fe345a29726d8fd257f57524b4a4082dc547cdb0
MirBytes <mirmutabassimali@gmail.com>
2025-04-12 16:55:15 +0500

    oopppa

--- 01_PYTHON/OOP.py
+++ 01_PYTHON/OOP.py
@@ -8,3 +8,23 @@ class person():
 
 ahmed = person("Ahmed", 22)
 ahmed.greet()
+
+class FLIGHT():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if len(self.passengers) < self.capacity:
+            self.passengers.append(name)
+            print(f"{name} has been added to the flight.")
+        else:
+            print("Sorry, the flight is full.")
+
+PIA = FLIGHT(5)
+PIA.add_passenger("Ahmed")
+PIA.add_passenger("Ali")
+PIA.add_passenger("Mir")
+PIA.add_passenger("Ibrahim")
+PIA.add_passenger("Jilani")
+PIA.add_passenger("RANA")
